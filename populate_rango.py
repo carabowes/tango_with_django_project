d29d3b9e91ec0931f47b67008d1bfe3d49636179
“Cara <2757542b@student.gla.ac.uk>
2024-02-06 14:41:59 +0000

    chapter 5 tests commit

--- populate_rango.py
+++ populate_rango.py
@@ -42,7 +42,7 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category, 
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
     
@@ -57,8 +57,10 @@ def add_page(cat, title, url, views=0):
     p.views=views
     p.save()
     return p
-def add_cat(name):
+def add_cat(name, views=0, likes = 0):
     c = Category.objects.get_or_create(name=name)[0] 
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 # Start execution here!
