f408230a099eafeaa5e6a4c32637aeccb6cd7f04
“Cara <2757542b@student.gla.ac.uk>
2024-02-07 18:51:54 +0000

    chapter 10 and testing  complete

--- rango/models.py
+++ rango/models.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
+    NAME_MAX_LENGTH = 128
+
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name) 
         super(Category, self).save(*args, **kwargs)
